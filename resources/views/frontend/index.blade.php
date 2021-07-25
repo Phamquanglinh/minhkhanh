579ea4c7d1dfdd216e8812d885ab6ce54a11a236
Phamquanglinh <Linh2002gv@gmail.com>
2021-07-25 11:10:40 +0700

    fix index load

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -27,12 +27,12 @@
         }
 
         .red-background {
-            background-image: url("{{asset("img/introduction/4K-red-bg.jpg")}}");
+            background-image: url("https://i.pinimg.com/originals/aa/4a/16/aa4a16851449aa6155f36686013bbb81.png");
             min-height: 500px;
         }
 
         .ideas-background {
-            background-image: url("{{asset("img/introduction/ideas-bg.jpg")}}");
+            background-image: url("https://i.pinimg.com/originals/aa/4a/16/aa4a16851449aa6155f36686013bbb81.png");
             min-height: 500px;
         }
 
